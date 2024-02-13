a5c346d4f04a585bab3cd09757255394c212d92c
Tim Köhler <149181237+ai-made-approachable@users.noreply.github.com>
2024-02-13 13:56:47 +0100

    Update replace_sql_placeholders.sh

--- replace_sql_placeholders.sh
+++ replace_sql_placeholders.sh
@@ -7,8 +7,8 @@ SQL_FILE="supabase/migrations/20240108234540_setup.sql"
 TEMP_FILE="temp.sql"
 
 # Environment variables (these should be set in your Railway environment or .env file)
-PROJECT_URL=${PROJECT_URL:-http://localhost:8000}
-SERVICE_ROLE_KEY=${SERVICE_ROLE_KEY:-your_default_service_role_key}
+PROJECT_URL=${NEXT_PUBLIC_SUPABASE_URL:-http://localhost:8000}
+SERVICE_ROLE_KEY=${SUPABASE_SERVICE_ROLE_KEY:-your_default_service_role_key}
 
 # Replace placeholders in the SQL file
 sed "s|PROJECT_URL_PLACEHOLDER|$PROJECT_URL|g; s|SERVICE_ROLE_KEY_PLACEHOLDER|$SERVICE_ROLE_KEY|g" $SQL_FILE > $TEMP_FILE
